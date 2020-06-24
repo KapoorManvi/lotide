7a7eb940b2d428724ff441dedba201145a575647
Manvi Kapoor <kapoor.manvi11@gmail.com>
2020-06-24 23:37:37 +0000

    updated with comments

--- letterPositions.js
+++ letterPositions.js
@@ -15,14 +15,17 @@ const assertArraysEqual = function(firstArray, secArray) {
   }
 } 
 
-
+// Indicate the index or position of each letter occurrence in a string and return in an object
 const letterPositions = function(sentence) {
+  // Collect occurrences 
   const results = {};
+  // Loop through each letter of a string
   for (let i = 0; i < sentence.length; i++) {
-
+    // if there is no space detected in the string
     if (sentence[i] !== ' ') {
+      // for each letter index
         if (results[sentence[i]]) {
-            //Number() <--- gets converted into a int or float
+            //Number() <--- gets converted into a int or float and add to results object
             results[sentence[i]].push(Number(i));
         } else {
             results[sentence[i]] = [Number(i)];
@@ -36,4 +39,7 @@ return results;
 
 
 // TEST CODE
+// Is "e" at index 1? Yes, so this is a positive test
 assertArraysEqual(letterPositions("hello").e, [1]);
+// Is "o" at index 1? No, so this is a negative test
+assertArraysEqual(letterPositions("hello").o, [1]);
