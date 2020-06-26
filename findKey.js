d5978c59fcb6e05285dc0e852ef0ff7de8385ab3
Manvi Kapoor <kapoor.manvi11@gmail.com>
2020-06-26 00:01:40 +0000

    Find a key when given a specific value with tests

---
+++ findKey.js
@@ -0,0 +1,55 @@
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`💔💔💔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Finds a key with a given value specified within the callback function
+const findKey = function(object, callback) {
+  //Loops through keys within passed object
+for (let item of Object.keys(object)) {
+  //If given value in callback found, return associated key
+  if (callback(object[item])) {
+    return item;
+  }
+}
+}
+
+//MANUAL VERIFICATION CODE 
+// console.log(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2));
+
+//TEST CODE
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+//Positive Test
+assertEqual(result1, "noma");
+//Negative Test
+assertEqual(result1, "Ora");
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
