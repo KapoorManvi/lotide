e83caf592ec91757dcc74f26b5cc59a6e1122326
Manvi Kapoor <kapoor.manvi11@gmail.com>
2020-06-30 06:31:36 +0000

    added mocha and chai and updated test cases

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,25 @@
 
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
+
+
+describe("#middle", () => {
+  it("returns empty array for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns empty array for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns empty array for [1, 2, 3,4 ,5 ,6]", () => {
+    assert.deepEqual(middle([1, 2, 3,4 ,5 ,6]), [3, 4]);
+  });  
+
+});
+
+
 
 
 //TEST CODE
